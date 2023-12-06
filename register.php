572af51f3299ca75d3a46f887ee7da8598ef07b5
Simonxf35 <77051141+Simonxf35@users.noreply.github.com>
2023-12-06 14:28:17 +0000

    changes made

---
+++ register.php
@@ -0,0 +1,40 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Collect form data
+    $username = $_POST["username"];
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+
+    // Validate password
+    if (strlen($password) < 8) {
+        die("Password must be at least 8 characters long.");
+    }
+
+    // Check for at least one uppercase letter
+    if (!preg_match("/[A-Z]/", $password)) {
+        die("Password must contain at least one uppercase letter.");
+    }
+
+    // Check for at least one lowercase letter
+    if (!preg_match("/[a-z]/", $password)) {
+        die("Password must contain at least one lowercase letter.");
+    }
+
+    // Check for at least one numerical digit
+    if (!preg_match("/[0-9]/", $password)) {
+        die("Password must contain at least one numerical digit.");
+    }
+
+    // Check for at least one special character
+    if (!preg_match("/[!@#$%^&*(),.?\":{}|<>]/", $password)) {
+        die("Password must contain at least one special character.");
+    }
+
+    // TODO: Implement email validation
+
+    // TODO: Store user information securely (e.g., use password hashing)
+
+    // Display a success message (for demonstration purposes)
+    echo "Sign-up successful! Welcome, $username!";
+}
+?>
