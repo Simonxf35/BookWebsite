338ca6b5fd12645a84e0cd76a4c115d92f5cd90d
Simonxf35 <77051141+Simonxf35@users.noreply.github.com>
2023-12-06 10:54:32 +0000

    Changes Made

---
+++ metadata.php
@@ -0,0 +1,6 @@
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta charset="UTF-8">
+<meta name="description" content="Free Web tutorials">
+<meta name="keywords" content="HTML, CSS, JavaScript">
+<meta name="author" content="Simon Mahon, Atasi Muppala, Christian Timofti">
