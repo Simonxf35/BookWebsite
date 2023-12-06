950c40c0b56e7239807d26204b003197f703a341
Simonxf35 <77051141+Simonxf35@users.noreply.github.com>
2023-12-06 14:40:58 +0000

    changes made

--- Users Table (.sql
+++ Users Table (.sql
@@ -1,6 +1,12 @@
 CREATE TABLE Users (
+    FirstName VARCHAR(100) NOT NULL,
+    middleName VARCHAR(100) NULL,
+    lastName VARCHAR(100) NOT NULL,
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(255) UNIQUE NOT NULL,
     EmailAddress VARCHAR(50) NOT NULL,
-    Password TEXT NOT NULL,
-    FullName VARCHAR(20) NOT NULL,
+    comfirmEmail VARCHAR(50) NOT NULL,
+    phone_number INT(50) NULL,
+    user_password VARCHAR(100) NOT NULL,
     Interest INT AUTO_INCREMENT NOT NULL PRIMARY KEY
 );
