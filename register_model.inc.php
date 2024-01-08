8505f989b20caa9a2d8a63d78209d1cc0fe10134
Simonxf35 <77051141+Simonxf35@users.noreply.github.com>
2024-01-08 15:52:10 +0000

    changes made

--- register_model.inc.php
+++ register_model.inc.php
@@ -1,39 +1,33 @@
 <?php
-declare (strict_types=1);
+declare(strict_types=1);
 
-function get_username(object $pdo, string $username){
+function get_username(PDO $pdo, string $username) {
     $query = "SELECT username FROM users WHERE username = :username;";
     $stmt = $pdo->prepare($query);
-    $stmt ->bindParam("username", $username);
+    $stmt->bindParam(":username", $username);
     $stmt->execute();
 
-    $result = $stmt->fetch(PDO::FETCH_ASSOC);
-    return $result;
+    return $stmt->fetch(PDO::FETCH_ASSOC);
 }
 
-function is_email_registered(object $pdo, string $email){
-    $query = "SELECT username FROM users WHERE email = email;";
+function is_email_registered(PDO $pdo, string $email) {
+    $query = "SELECT email FROM users WHERE email = :email;";
     $stmt = $pdo->prepare($query);
-    $stmt ->bindParam(":email", $email);
+    $stmt->bindParam(":email", $email);
     $stmt->execute();
 
-    $result = $stmt->fetch(PDO::FETCH_ASSOC);
-    return $result;
+    return $stmt->fetch(PDO::FETCH_ASSOC);
 }
 
-function set_user(object $pdo, string $password, string $username, string $email): {
-    $query = "INSERT INTO users (username, email, password) VALUES
-    (:username, :$password , :$username);";
+function set_user(PDO $pdo, string $username, string $email, string $password) {
+    $query = "INSERT INTO users (username, email, password) VALUES (:username, :email, :password);";
     $stmt = $pdo->prepare($query);
 
-    $options = [
-        "cost" => 12
-    ];
-    $hashedpassword = password_hash($password, $PASSWORD_BCRPT, $options);
+    $options = ["cost" => 12];
+    $hashedPassword = password_hash($password, PASSWORD_BCRYPT, $options);
 
-
-    $stmt ->bindParam(":$username", $username);
-    $stmt ->bindParam(":password", $password);
-    $stmt ->bindParam(":email", $email);
+    $stmt->bindParam(":username", $username);
+    $stmt->bindParam(":email", $email);
+    $stmt->bindParam(":password", $hashedPassword);
     $stmt->execute();
 }
