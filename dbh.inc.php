7c3c3d76f45d16975e064a5ab49589c578f56721
Simonxf35 <77051141+Simonxf35@users.noreply.github.com>
2024-01-08 18:45:44 +0000

    changes made

--- dbh.inc.php
+++ dbh.inc.php
@@ -1,16 +1,15 @@
 <?php
 
 $host = 'localhost';
-$dbname = '';
-$username = 'root';
-$dbpassword = '';
+$dbname = 'websitedatabase'; // Name of your database
+$username = 'root'; // Your database username
+$dbpassword = 'password123'; // Your database password
 
-try{
-    $pdo = new PDO("mysql:host=host;dbname=$dbname", $dbusername, 
-    $dbpassword);
-    $pdo ->setAttribute(\PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch(PDOException $e) {
-    die("Could not connect to database" .$e->getMessage());
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $dbpassword);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Could not connect to the database: " . $e->getMessage());
 }
 
 ?>
