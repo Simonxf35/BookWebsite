ba24019efb82ff3e3e239a2741967c0daa408415
Simonxf35 <77051141+Simonxf35@users.noreply.github.com>
2024-01-23 15:26:32 +0000

    Update dbh.inc.php

--- dbh.inc.php
+++ dbh.inc.php
@@ -1,9 +1,9 @@
 <?php
 
 $host = 'localhost';
-$dbname = 'websitedatabase'; // Name of your database
-$username = 'root'; // Your database username
-$dbpassword = 'password123'; // Your database password
+$dbname = 'websitedatabase';
+$username = 'root';
+$dbpassword = 'password123'; 
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $dbpassword);
