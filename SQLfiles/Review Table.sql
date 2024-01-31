b3d7dff5d5c6a1c7b4771791504401bfe3e324b2
Simonxf35 <77051141+Simonxf35@users.noreply.github.com>
2024-01-31 13:50:31 +0000

    Update and rename Review Table to Review Table.sql

--- SQLfiles/Review Table.sql
+++ SQLfiles/Review Table.sql
@@ -6,5 +6,7 @@ CREATE TABLE reviews (
     review TEXT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (book_id) REFERENCES books(id),
-    FOREIGN KEY (user_id) REFERENCES users(id)
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    INDEX (book_id),
+    INDEX (user_id)
 );
